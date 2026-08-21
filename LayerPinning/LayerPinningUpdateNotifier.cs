f472de60102f90b3a9533cb0fe08b2ebacf867b9
routersys <github.routersys@gmail.com>
2026-08-21 23:53:50 +0900

    非推奨YMM4バージョンの警告表示を追加

--- LayerPinning/LayerPinningUpdateNotifier.cs
+++ LayerPinning/LayerPinningUpdateNotifier.cs
@@ -1,6 +1,9 @@
+using System.Net.Http;
 using System.Windows;
 using GitHubUpdateNotifier;
+using Newtonsoft.Json.Linq;
 using YukkuriMovieMaker.Plugin.Update;
+using YukkuriMovieMaker.Project.Items;
 using Notifier = GitHubUpdateNotifier.GitHubUpdateNotifier;
 
 namespace LayerPinning
@@ -9,6 +12,7 @@ namespace LayerPinning
     {
         private const string Owner = "routersys";
         private const string Repository = "YMM4-LayerPinning";
+        private const string CompatibilityUrl = "https://raw.githubusercontent.com/routersys/YMM4-LayerPinning/main/.github/compatibility.json";
 
         private static int started;
 
@@ -37,19 +41,46 @@ namespace LayerPinning
             catch
             {
             }
+            try
+            {
+                await NotifyUnsupportedYmm4Async().ConfigureAwait(false);
+            }
+            catch
+            {
+            }
+        }
+
+        private static async Task NotifyUnsupportedYmm4Async()
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(Repository);
+            var json = await client.GetStringAsync(CompatibilityUrl).ConfigureAwait(false);
+            var compatibility = JObject.Parse(json);
+            if (compatibility.Value<bool>("isLatestSupported"))
+                return;
+            if (!Version.TryParse(compatibility.Value<string>("lastGoodYmm4"), out var lastGoodVersion))
+                return;
+            var currentVersion = typeof(GroupItem).Assembly.GetName().Version;
+            if (currentVersion is null || currentVersion <= lastGoodVersion)
+                return;
+            var message = string.Format(Texts.UnsupportedYmm4Message, currentVersion, lastGoodVersion);
+            await ShowMessageBoxAsync(message, MessageBoxImage.Warning).ConfigureAwait(false);
         }
 
         private static string FormatMessage(UpdateInfo update)
             => string.Format(Texts.UpdateAvailableMessage, update.TagName) + Environment.NewLine + update.ReleaseUrl;
 
         private static Task ShowNotificationAsync(UpdateNotification notification, CancellationToken cancellationToken)
+            => ShowMessageBoxAsync(notification.Message, MessageBoxImage.Information);
+
+        private static Task ShowMessageBoxAsync(string message, MessageBoxImage image)
         {
             var application = Application.Current;
             if (application is null)
                 return Task.CompletedTask;
 
             return application.Dispatcher.InvokeAsync(() =>
-                MessageBox.Show(notification.Message, Texts.LayerPinningPluginName, MessageBoxButton.OK, MessageBoxImage.Information)).Task;
+                MessageBox.Show(message, Texts.LayerPinningPluginName, MessageBoxButton.OK, image)).Task;
         }
     }
 }
